d63ebc721774ef421cb442ba5ce04c48db3631f5
some random idiot <sittipat2554aa@hotmail.com>
2022-02-12 16:43:34 +0700

    Removed PUT method.

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ class Record(BaseModel):
     name: Optional[str]
     start_time: Optional[datetime.time]
     current_duration: Optional[datetime.timedelta]
-    room: Optional[int]
+    room: int
 
 
 client = MongoClient('mongodb://localhost', 27017)
@@ -84,73 +84,45 @@ def get_room(room: int):
 
 
 @app.post("/create-room")
-def create_room(record: Record):
+async def create_room(record: Record):
     """Creates a new record in the database.
     This is used when there are no previous records of a room, or the latest record has already ended.
     It automatically creates timestamp and duration.
 
     Example request:
     {
-        "status": true,
+        "status": 1,
         "name": "John Doe",
-        "room": 1
+        "room": 2
     }
     """
-    record = jsonable_encoder(record)
-    if not record["status"]:
-        return {"status": "failure",
-                "message": "Initial status can't be 0."}
-
-    record["status"] = 1
-    record["start_time"] = str(datetime.datetime.now().time().strftime("%H:%M:%S"))
-    record["current_duration"] = str(datetime.timedelta(0))
-    collection.insert_one(record)
-    return {"status": "success"}
-
-
-@app.put("/update-room/{room}")
-async def update_room(room: int, request: Request):
-    """Updates the latest record of a room in the database.
-    If the latest record's 'status' is False, then the method rejects the update.
-
-    Example request:
-    {
-        "status": false,
-    }
-    """
-    attributes = await request.json()
-    if "status" in attributes:
-        if attributes["status"]:
-            return {"status": "failure",
-                    "message": "Status for updating a room information can only be 0. Use create-room otherwise."}
-
-    if collection.find_one({"room": room}):
+    if record["status"] == 1 or collection.find_one({"room": record["room"]}) is None:
+        record = jsonable_encoder(record)
+        record["status"] = 1
+        record["start_time"] = str(datetime.datetime.now().time().strftime("%H:%M:%S"))
+        record["current_duration"] = str(datetime.timedelta(0))
+        collection.insert_one(record)
+        return {"status": "success"}
+    elif record["status"] == 0:
         # Get the latest record.
-        records = collection.find({"room": room})
+        records = collection.find({"room": record["room"]}, {"_id": 0})
         record_list = []
         for record in records:
             record_list.append(record)
         target_record = record_list[-1]
 
-        if target_record["status"]:
-            # Check if the attributes are valid.
-            has_attribute = False
-            for attribute in attributes:
-                has_attribute = attribute in target_record
+        # Check if the attributes are valid.
+        has_attribute = False
+        for attribute in record:
+            has_attribute = attribute in target_record
 
-            if has_attribute:
-                collection.update_one({"_id": target_record["_id"]}, {"$set": attributes})
-                # Update the duration.
-                current_duration = str(datetime.datetime.strptime(datetime.datetime.now().time().strftime("%H:%M:%S"), "%H:%M:%S")
-                                       - datetime.datetime.strptime(target_record["start_time"], "%H:%M:%S"))
-                collection.update_one({"_id": target_record["_id"]}, {"$set": {"current_duration": current_duration}})
-                return {"status": "success"}
-            else:
-                return {"status": "failure",
-                        "message": "No such attribute detected. Please check the request body."}
+        if has_attribute:
+            collection.update_one({"_id": target_record["_id"]}, {"$set": record})
+            # Update the duration.
+            current_duration = str(datetime.datetime.strptime(datetime.datetime.now().time().strftime("%H:%M:%S"), "%H:%M:%S")
+                                    - datetime.datetime.strptime(target_record["start_time"], "%H:%M:%S"))
+            collection.update_one({"_id": target_record["_id"]}, {"$set": {"current_duration": current_duration}})
+            return {"status": "success"}
         else:
             return {"status": "failure",
-                    "message": "The latest record has already ended. Please create a new record instead."}
-    else:
-        return {"status": "failure",
-                "message": f"No previous record(s) of room {room} found. Please create a new record."}
+                    "message": "No such attribute detected. Please check the request body."}
