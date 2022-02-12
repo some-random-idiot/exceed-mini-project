a19472b3b098e968f6bd04985ce6f66c0ea9d2d1
some random idiot <sittipat2554aa@hotmail.com>
2022-02-12 14:04:37 +0700

    Implemented POST and PUT methods.

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,18 @@
-from xmlrpc.client import DateTime, boolean
-from fastapi import FastAPI
+import datetime
+from xmlrpc.client import boolean
+from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
-from numpy import int16
 
 from pymongo import MongoClient
 from pydantic import BaseModel
 
 
-class Database(BaseModel):
-    status: boolean
+class Record(BaseModel):
+    status: boolean  # True if the record is active (someone is in the restroom).
     name: str
-    start_time: DateTime
-    end_time: DateTime
-    current_duration: int
+    start_time: datetime.time
+    end_time: datetime.time
+    current_duration: datetime.timedelta
     room: int
 
 
@@ -22,3 +22,63 @@ db = client["mini_project"]
 collection = db["database"]
 
 app = FastAPI()
+
+
+@app.post("/create-record")
+def create_record(record: Record):
+    """Creates a new record in the database.
+    This is used when there are no previous records of a room or the latest record has already ended.
+
+    Example request:
+    {
+        "status": true,
+        "name": "John Doe",
+        "start_time": "10:00",
+        "end_time": "11:00",
+        "current_duration": "00:00:00",
+        "room": 1
+    }
+    """
+    record = jsonable_encoder(record)
+    if record["status"]:
+        collection.insert_one(record)
+        return {"status": "success"}
+    else:
+        return {"status": "failure",
+                "message": "The record's initial status can't be false."}
+
+
+@app.put("/update-record/{room}")
+async def update_record(room: int, request: Request):
+    """Updates the latest record of a room in the database.
+    If a record's 'status' is False, it rejects the update.
+
+    Example request:
+    {
+        "status": false,
+        "end_time": "11:00",
+        "current_duration": "00:01:00"
+    }
+    """
+    attributes = await request.json()
+    if "name" in attributes:
+        return {"status": "failure",
+                "message": "Name update attempt detected. Please create a new record instead."}
+
+    if collection.find_one({"room": room}):
+        records = collection.find({"room": room})
+        record_list = []
+        for record in records:
+            record_list.append(record)
+        target_record = record_list[-1]  # Get the latest record.
+
+        if target_record["status"]:
+            collection.update_one({"_id": target_record["_id"]}, {"$set": attributes})
+            return {"status": "success",
+                    "message": f"Updated:\\n {attributes}"}
+        else:
+            return {"status": "failure",
+                    "message": "The latest record has already ended. Please create a new record instead."}
+    else:
+        return {"status": "failure",
+                "message": f"No previous record of room {room} found. Please create a new record."}
