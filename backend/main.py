a3a480752212c75141454be8d2f56eda446092ff
Wha Chanunya <whachanunya@gmail.com>
2022-02-12 13:49:46 +0700

    add average

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from sqlite3 import Time
 from xmlrpc.client import DateTime, boolean
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-from pandas import Timedelta
+from numpy import average, size
 
 from pymongo import MongoClient
 from pydantic import BaseModel
@@ -13,7 +13,7 @@ class Record(BaseModel):
     status: boolean
     start_time: datetime.time
     end_time: datetime.time
-    duration: datetime.timedelta
+    duration: int
     room: int
 
 
@@ -25,14 +25,28 @@ collection = db["database"]
 
 app = FastAPI()
 
+def ave(room):
+    result = collection.find({"room": room}, {"_id": 0})
+    result_list = []
+    for record in result:
+        result_list.append(record["duration"])
+    return sum(result_list)/len(result_list)
+
 @app.get("/get-room/{room}")
 def get_room(room: int):
     result = collection.find_one({"room": room}, {"_id": 0})
-    if result:
+    if result["status"] == 'true':
         return {
                 "room": result["room"],
                 "status": result["status"],
-                "duration": result["current_duration"]
+                "average": ave(room)
+                }
+    elif result["status"] == 'false':
+        return {
+                "room": result["room"],
+                "status": result["status"],
+                "start_time": result["start_time"],
+                "average": ave(room)
                 }
     return {
         "room": room,
