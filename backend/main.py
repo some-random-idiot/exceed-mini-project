bfc01a50b3c6a904031a554efab6bbf70a5fee1b
Wha Chanunya <whachanunya@gmail.com>
2022-02-12 13:04:29 +0700

    get

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,19 @@
+import datetime
+from sqlite3 import Time
 from xmlrpc.client import DateTime, boolean
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
-from numpy import int16
+from pandas import Timedelta
 
 from pymongo import MongoClient
 from pydantic import BaseModel
 
 
-class Database(BaseModel):
+class Record(BaseModel):
     status: boolean
-    start_time: DateTime
-    end_time: DateTime
-    current_duration: int
+    start_time: datetime.time
+    end_time: datetime.time
+    duration: datetime.timedelta
     room: int
 
 
@@ -20,4 +22,19 @@ client = MongoClient('mongodb://localhost', 27017)
 db = client["mini_project"]
 collection = db["database"]
 
+
 app = FastAPI()
+
+@app.get("/get-room/{room}")
+def get_room(room: int):
+    result = collection.find_one({"room": room}, {"_id": 0})
+    if result:
+        return {
+                "room": result["room"],
+                "status": result["status"],
+                "duration": result["current_duration"]
+                }
+    return {
+        "room": room,
+        "status": True
+    }
